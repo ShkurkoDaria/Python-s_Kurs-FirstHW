ef58277f7bf6b644cb29f5c57071021937ebc012
ShkurkoDaria <ShkurkoDV@yandex.ru>
2015-11-04 17:51:12 +0300

    Update dz4.py

--- dz4.py
+++ dz4.py
@@ -13,12 +13,27 @@ def outputListToFilePickle(listOfCar, fileName):
     pickle.dump(listOfCar,open(fileName,"wb"),protocol=3)
     return 0
 
+def inputNewCar(listOfCar):
+    #Происходит добавление нового автомобиля в файл
+    inputStrCarName = input("Введите имя автомобиля:")
+    inputStrCarPower = input("Введите мощность автомобиля:")
+    if inputStrCarPower.isdigit():
+        listOfCar.append(inputStrCarName + " " + inputStrCarPower)
+    else:
+        print("Ошибка ввода!\n")
+    return 0
+
+def outputAllCar(listOfCar):
+    #Вывод списка всех автомобилей, которые входят в файл
+    listOfCar.sort()
+    print(listOfCar)
+    return 0
+
 def inputCommand():
     command = ""
     #Предложить пользователю выбрать одно из возможных действий
     userChange = input("Что делать:\n 1.Добавить новый автомобиль?\n"
                        " 2.Вывести все автомобили?\n 3.Вывести автомобили по условию?\n")
-                       
     if userChange == "1":
         command += userChange
     elif userChange == "2":
@@ -50,6 +65,61 @@ def inputCommand():
             command += userChangeFilter
     return command
 
+def search1_SpecificNumber(listOfCar):
+#При вводе конкретного числа
+    dreamPower = input("Введите желаемую мощность\n")
+    for car in listOfCar:
+        carPower = int(car[car.find(' '):])
+        if carPower == int(dreamPower):
+            print(car)
+    return 0
+
+def search1_More(listOfCar):
+#при вводе числа больше заданного
+    dreamPower = input("Введите желаемую мощность\n")
+    for car in listOfCar:
+        carPower = int(car[car.find(' '):])
+        if carPower > int(dreamPower):
+            print(car)
+    return 0
+
+def search1_Less(listOfCar):
+#при вводе числа меньше заданного
+    dreamPower = input("Введите желаемую мощность\n")
+    for car in listOfCar:
+        carPower = int(car[car.find(' '):])
+        if carPower < int(dreamPower):
+            print(car)
+    return 0
+
+def search1_Interval(listOfCar):
+#При вводе желаемого промежутка
+    dreamPowerMax = input("Введите максимальную мощность:\n")
+    dreamPowerMin = input("Введите минимальную мощность:\n")
+    for car in listOfCar:
+        carPower = int(car[car.find(' '):])
+        if carPower >= int(dreamPowerMin) and carPower <= int(dreamPowerMax):
+            print(car)
+    return 0
+
+def search2(listOfCar):
+    wordName = input("Введите слово: \n")
+    for car in listOfCar:
+        carName = car[:car.find(' ')]
+        if carName.find(wordName) > -1:
+            print(car)
+    return 0
+
+def search3(listOfCar):
+#Производим поиск автомобиля по полному соответствию слова
+    wordName = input("Введите слово: \n")
+    for car in listOfCar:
+        carName = car[:car.find(' ')]
+        if carName == wordName:
+            print(car)
+    return 0
+
+
 continueFlag = True
 
 listOfCar = []
@@ -59,72 +129,32 @@ while continueFlag:
     myCommand = inputCommand()
 
     if myCommand[0] == "1":
-        #Происходит добавление нового автомобиля в файл
-        inputStrCarName = input("Введите имя автомобиля:")
-        inputStrCarPower = input("Введите мощность автомобиля:")
-
-        if inputStrCarPower.isdigit():
-            listOfCar.append(inputStrCarName + " " + inputStrCarPower)
-        else:
-            print("Ошибка ввода!\n")
+        inputNewCar(listOfCar)
 
     elif myCommand[0] == "2":
-        #Вывод списка всех автомобилей, которые входят в файл
-        listOfCar.sort()
-        print(listOfCar)
+        outputAllCar(listOfCar)
 
     elif myCommand[0] == "3":
         if myCommand[1] == "1":
             if myCommand[2] == "1":
-            #При вводе конкретного числа
-                dreamPower = input("Введите желаему мощность\n")
-                for car in listOfCar:
-                    carPower = int(car[car.find(' '):])
-                    if carPower == int(dreamPower):
-                        print(car)
+                search1_SpecificNumber(listOfCar)
 
             elif myCommand[2] == "2":
-            #при вводе числа больше заданного
-                dreamPower = input("Введите желаему мощность\n")
-                for car in listOfCar:
-                    carPower = int(car[car.find(' '):])
-                    if carPower > int(dreamPower):
-                        print(car)
+                search1_More(listOfCar)
 
             elif myCommand[2] == "3":
-            #при вводе числа меньше заданного
-                dreamPower = input("Введите желаему мощность\n")
-                for car in listOfCar:
-                    carPower = int(car[car.find(' '):])
-                    if carPower < int(dreamPower):
-                        print(car)
+                search1_Less(listOfCar)
 
             elif myCommand[2] == "4":
-            #При вводе желаемого промежутка
-                dreamPowerMax = input("Введите максимальную мощность:\n")
-                dreamPowerMin = input("Введите минимальную мощность:\n")
-                for car in listOfCar:
-                    carPower = int(car[car.find(' '):])
-                    if carPower >= int(dreamPowerMin) and carPower <= int(dreamPowerMax):
-                        print(car)
+                search1_Interval(listOfCar)
 
 
         elif myCommand[1] == "2":
-        #Производим поиск по вхождению слова в название
-            wordName = input("Введите слово: \n")
-            for car in listOfCar:
-                carName = car[:car.find(' ')]
-                if carName.find(wordName) > -1:
-                    print(car)
+            search2(listOfCar)
 
 
         elif myCommand[1] == "3":
-        #Производим поиск автомобиля по полному соответствию слова
-            wordName = input("Введите слово: \n")
-            for car in listOfCar:
-                carName = car[:car.find(' ')]
-                if carName == wordName:
-                    print(car)
+            search3(listOfCar)
 
     if input("Продолжить работу? да(1)/нет(0)?\n") == "1":
         continue
